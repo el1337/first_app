6088f73a961f434d2cfea781df1de91f545c117a
el1337 <e.luttenberger@web.de>
2011-04-11 16:38:40 +0200

    Initial commit

---
+++ hallowelt/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hallowelt::Application.config.secret_token = 'a4fdb0cf9bc03a63d48b3424028d3677d416ce482b810a527064173bd4aa46baa8e3366572ffce558a5a62a98a13281ff75d8158ed53cccd556c61f45db0a3f0'
